768ac08ead2e8af7acf68cdd08273e3ee71e930e
Malte Wessel <malte-wessel@users.noreply.github.com>
2020-05-04 08:41:26 +0200

    tweaks

--- src/hooks/useLoadMapsApi.ts
+++ src/hooks/useLoadMapsApi.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import { useIsMounted } from './useIsMounted';
 
 const initiCallbackName = 'googleMapsInitCallback';
